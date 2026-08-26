6c7a44dee97be1aa5188bec3222f90cb2ffa156d
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2026-08-26 19:13:10 +0500

    Add dist/index.d.ts with pagination and high performance enhancements

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export { TelegramUserBotApi } from './credentials/TelegramUserBotApi.credentials';
+export { TelegramUserBot } from './nodes/TelegramUserBot/TelegramUserBot.node';
+export { TelegramUserBotTrigger } from './nodes/TelegramUserBot/TelegramUserBotTrigger.node';
+export { TelegramUserBotHealthCheck } from './nodes/TelegramUserBot/TelegramUserBotHealthCheck.node';
+//# sourceMappingURL=index.d.ts.map
