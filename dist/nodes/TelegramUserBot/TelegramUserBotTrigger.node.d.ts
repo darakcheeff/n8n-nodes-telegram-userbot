b69b2b0d3006f9fa486c92d02474fee51ea3e315
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2026-08-26 19:13:24 +0500

    Add dist/nodes/TelegramUserBot/TelegramUserBotTrigger.node.d.ts with pagination and high performance enhancements

---
+++ dist/nodes/TelegramUserBot/TelegramUserBotTrigger.node.d.ts
@@ -0,0 +1,6 @@
+import { ITriggerFunctions, INodeType, INodeTypeDescription, ITriggerResponse } from 'n8n-workflow';
+export declare class TelegramUserBotTrigger implements INodeType {
+    description: INodeTypeDescription;
+    trigger(this: ITriggerFunctions): Promise<ITriggerResponse>;
+}
+//# sourceMappingURL=TelegramUserBotTrigger.node.d.ts.map
