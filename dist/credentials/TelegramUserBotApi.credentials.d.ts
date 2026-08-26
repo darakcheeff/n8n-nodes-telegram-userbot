2695300dfcb0a18557cdff1552c0231e4f51480f
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2026-08-26 19:13:16 +0500

    Add dist/credentials/TelegramUserBotApi.credentials.d.ts with pagination and high performance enhancements

---
+++ dist/credentials/TelegramUserBotApi.credentials.d.ts
@@ -0,0 +1,8 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+export declare class TelegramUserBotApi implements ICredentialType {
+    name: string;
+    displayName: string;
+    documentationUrl: string;
+    properties: INodeProperties[];
+}
+//# sourceMappingURL=TelegramUserBotApi.credentials.d.ts.map
