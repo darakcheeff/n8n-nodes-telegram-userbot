6fde25e99b07acaea4ad16e626a8b812233ccddd
darakcheeff <30556307+darakcheeff@users.noreply.github.com>
2026-08-26 19:13:39 +0500

    Add dist/nodes/TelegramUserBot/TelegramUserBotHealthCheck.node.d.ts with pagination and high performance enhancements

---
+++ dist/nodes/TelegramUserBot/TelegramUserBotHealthCheck.node.d.ts
@@ -0,0 +1,6 @@
+import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
+export declare class TelegramUserBotHealthCheck implements INodeType {
+    description: INodeTypeDescription;
+    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
+}
+//# sourceMappingURL=TelegramUserBotHealthCheck.node.d.ts.map
